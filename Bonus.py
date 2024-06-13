70e5667c453e43fa03d00fc9e1bc9268efaf0abd
Sarah <monifsarah@gmail.com>
2024-06-13 22:32:38 +0300

    Moved exception  handling into main function for better organization

--- Bonus.py
+++ Bonus.py
@@ -8,11 +8,6 @@
         TypeError: If the user enters an invalid unit (not 'C' or 'F').
         ValueError: If the user enters a non-numeric value for the temperature.
 """
-
-class TypeError(Exception):
-   
-   """ If the user enters an invalid unit (not 'C' or 'F') """
-
 def fahrenheit_to_celsius(fahrenheit):
    celsius = round(((fahrenheit - 32) * 5/9),2)
    return celsius
@@ -22,38 +17,37 @@ def celsius_to_fahrenheit(celsius):
   
 
 def main():
-  
-    
-    temperature_and_unit = input("Enter a temperature and its unit (e.g., \"25 C\" or \"77 F\"): ")
-
-    input_splited=temperature_and_unit.split(" ")
-     
-    if input_splited[1].upper()!="F" :
-        if input_splited[1].upper()!="C" :
-         raise TypeError(" Please enter a vaild Type  eg( C ,F) ")
-    
-    if input_splited[1].upper()=="F":
-     fahrenheit=input_splited[0]
-     Fahrenheit=float(fahrenheit)
-     celsius=fahrenheit_to_celsius(Fahrenheit)
-     print(f"Temperature in Celsius: {celsius} C")
-
-    if input_splited[1].upper()=="C":
-     celsius=input_splited[0]
-     Celsius=float(celsius)
-     fahrenheit=celsius_to_fahrenheit(Celsius)
-     print(f"Temperature in fahrenheit: {fahrenheit} F")
-
-
-while True:
+ while True:  
     try:
-        main()
+   
+     temperature_and_unit = input("Enter a temperature and its unit (e.g., \"25 C\" or \"77 F\"): ")
+
+     input_splited=temperature_and_unit.split(" ")
+     
+     if input_splited[1].upper()!="F" :
+         if input_splited[1].upper()!="C" :
+          raise TypeError
+    
+     if input_splited[1].upper()=="F":
+      fahrenheit=input_splited[0]
+      Fahrenheit=float(fahrenheit)
+      celsius=fahrenheit_to_celsius(Fahrenheit)
+      print(f"Temperature in Celsius: {celsius} C")
+
+     if input_splited[1].upper()=="C":
+      celsius=input_splited[0]
+      Celsius=float(celsius)
+      fahrenheit=celsius_to_fahrenheit(Celsius)
+      print(f"Temperature in fahrenheit: {fahrenheit} F")
+       
     except ValueError as e :
        print(f"{e}  Please enter a number  ")
        
     except TypeError as e :
-       print(e)
+       print(f"TypeError !!  Please enter a vaild Type  eg( C ,F) ")
     except Exception as e :
        print(e) 
 
+
+main()
 
