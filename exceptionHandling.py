c6413e55539409382fa02fdb5174dfb7351fd74b
D. F <ifonei@hotmail.com>
2024-06-13 15:28:54 +0300

    exception handling done

---
+++ exceptionHandling.py
@@ -0,0 +1,11 @@
+def additoin(x, y):
+    x = 10
+    y = 20
+    print("Addition:", x + b)
+    print("the operation is successful")
+
+try:
+    additoin(10, 20)
+except NameError:
+    print("Please make sure the variable are all defined.")
+
