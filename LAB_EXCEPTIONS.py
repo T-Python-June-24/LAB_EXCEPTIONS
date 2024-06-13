593a39895fe48d504036ce2c625df21a2005b81e
Abdulrahman-Alwagdani <167430546+Abdulrahman-Alwagdani@users.noreply.github.com>
2024-06-13 13:16:36 +0300

    Completed LAB_EXCEPTIONS

---
+++ LAB_EXCEPTIONS.py
@@ -0,0 +1,11 @@
+def additoin(x, y):
+    try:
+        x = 10
+        y = 20
+        print("Addition:", x + b)
+    except Exception as e:
+        print(e.__class__)
+        print("Something went wrong")
+    else:
+        print("the operation is successful")
+additoin(10, 20)
