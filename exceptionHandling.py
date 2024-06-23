ae92ff4e4597c0a66ffaff0951ab6c3c5bd16949
D. F <ifonei@hotmail.com>
2024-06-23 09:00:07 +0300

    improvemrnt

--- exceptionHandling.py
+++ exceptionHandling.py
@@ -2,10 +2,10 @@ def additoin(x, y):
     x = 10
     y = 20
     print("Addition:", x + b)
-    print("the operation is successful")
 
 try:
     additoin(10, 20)
 except NameError:
     print("Please make sure the variable are all defined.")
-
+else:
+    print("the operation is successful")
